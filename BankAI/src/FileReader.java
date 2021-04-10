d0b8aedfc7e15f030f33833b44b076cc5fae948f
Hadeel Alhajri <xhadeel246@gmail.com>
2021-04-10 14:23:28 +0300

    Comments for file reader

--- BankAI/src/FileReader.java
+++ BankAI/src/FileReader.java
@@ -52,35 +52,13 @@ public class FileReader {
   // --------------------------------------------------------------------------------------------------------------------
   /*
    * The method is used to read the tree elements from a file, then returns an
-   * aaray of string elements each element is either "0" or "null" at the begining
-   * Note: This methos was used for the second version of the project
+   * aaray of characters elements each element is either '0' or null at the
+   * begining Note: This methos was used for the second version of the project
    */
-  public static String[] fromFileToArray(String fileName) {
-    String[] floor = null;
-    try {
-      File file = new File("src/" + fileName + ".txt");
-      Scanner scanner = new Scanner(file);
-      if (scanner.hasNextLine()) {
-        String str = scanner.nextLine();
-        String floorMapString = str.substring(1, str.length() - 1);
-        floor = floorMapString.split(",");
-      }
-      scanner.close();
-
-    } catch (FileNotFoundException e) {
-      System.out.println("There is an error while reading the " + fileName + " file !");
-      e.printStackTrace();
-    }
-    return floor;
-  }
-
-  // --------------------------------------------------------------------------------------------------------------------
-
   public static Character[] fromFileToCharacterArray(String fileName) {
     String[] floorString = null;
     try {
       File file = new File("src/" + fileName + ".txt");
-      System.out.println(file.getAbsolutePath());
       Scanner scanner = new Scanner(file);
       if (scanner.hasNextLine()) {
         String str = scanner.nextLine();
@@ -95,13 +73,11 @@ public class FileReader {
     }
     Character[] floor = new Character[floorString.length];
     for (int i = 0; i < floor.length; i++) {
-
       if (floorString[i].equals("0")) {
         floor[i] = floorString[i].charAt(0);
       } else {
         floor[i] = null;
       }
-
     }
     return floor;
   }
